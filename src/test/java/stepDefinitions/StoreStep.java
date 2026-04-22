2d71bb1b0c635722deb26386dfe1e397806fbe67
AbishekBharathD <abishek22092004@gmail.com>
2026-04-22 14:28:00 +0530

    Adding dataTables and scenario outlines

--- src/test/java/stepDefinitions/StoreStep.java
+++ src/test/java/stepDefinitions/StoreStep.java
@@ -1,8 +1,6 @@
 package stepDefinitions;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import base.BaseTest;
@@ -14,178 +12,152 @@ import io.restassured.response.Response;
 import pojo.Order;
 import utils.ExcelUtility;
 
-public class StoreStep extends BaseTest{
+public class StoreStep extends BaseTest {
 
 	Response response;
-	static List<Integer> orderIds = new ArrayList<>();
-	
+	int orderId;
+
 	@When("user sends GET request to fetch store inventory")
 	public void getInventory() {
-		response = RestAssured
-								.given()
-									.spec(requestSpec)
-								.when()
-									.get(IPetStoreEndpoint.GET_INVENTORY);
-		
+		response = RestAssured.given().spec(requestSpec).when().get(IPetStoreEndpoint.GET_INVENTORY);
+
 		response.then().log().all();
-		
+
 		Assertions.setResponse(response);
-		
+
 	}
-	
-	@When("user sends POST request to place order")
-	public void createOrderWithFullBody() {
-		
+
+	@When("user sends POST request to place order with {int}")
+	public void createOrderWithFullBody(int rowNum) {
+
 		Object[][] data = ExcelUtility.getSheetData("testData.xlsx", "StoreModule");
-		for(int rowNum=0; rowNum<data.length; rowNum++) {
-		    String id = data[rowNum][0].toString();
-		    String petId = data[rowNum][1].toString();
-		    String quantity = data[rowNum][2].toString();
-		    String shipDate = data[rowNum][3].toString();
-		    String status = data[rowNum][4].toString();
-		    String complete = data[rowNum][5].toString();
-	
-		    Order order = new Order();
-		    order.setId((int) Double.parseDouble(id));
-		    order.setPetId((int) Double.parseDouble(petId));
-		    order.setQuantity((int) Double.parseDouble(quantity));
-		    order.setShipDate(shipDate);
-		    order.setStatus(status);
-		    order.setComplete(Boolean.parseBoolean(complete));
-				
-				response = RestAssured
-									.given()
-										.spec(requestSpec)
-										.body(order)
-									.when()
-										.post(IPetStoreEndpoint.CREATE_ORDER);
-				
-				response.then().log().all();
-				
-				Assertions.setResponse(response);
-				int orderId = response.jsonPath().getInt("id");
-				orderIds.add(orderId);
-				
-		}
+		String id = data[rowNum][0].toString();
+		String petId = data[rowNum][1].toString();
+		String quantity = data[rowNum][2].toString();
+		String shipDate = data[rowNum][3].toString();
+		String status = data[rowNum][4].toString();
+		String complete = data[rowNum][5].toString();
+
+		Order order = new Order();
+		order.setId((int) Double.parseDouble(id));
+		order.setPetId((int) Double.parseDouble(petId));
+		order.setQuantity((int) Double.parseDouble(quantity));
+		order.setShipDate(shipDate);
+		order.setStatus(status);
+		order.setComplete(Boolean.parseBoolean(complete));
+
+		response = RestAssured.given().spec(requestSpec).body(order).when().post(IPetStoreEndpoint.CREATE_ORDER);
+
+		response.then().log().all();
+
+		Assertions.setResponse(response);
+		orderId = response.jsonPath().getInt("id");
+
 	}
-	
+
 	@When("user sends POST request to place order with mandatory field")
 	public void createOrderWithMandatoryField() {
-		response = RestAssured
-								.given()
-									.spec(requestSpec)
-									.body("{\r\n"
-											+ "  \"id\": 10002,\r\n"
-											+ "  \"petId\": 101,\r\n"
-											+ "  \"quantity\": 3\r\n"
-											+ "}")
-								.when()
-									.post(IPetStoreEndpoint.CREATE_ORDER);
-		
-		response.then().log().all();
-		Assertions.setResponse(response);
-	}
-	
-	@When("user sends GET request to get order by existing id")
-	public void getOrderByExistingId() {
-		
-		for(Integer orderId : orderIds) {
-				response = RestAssured
-										.given()
-											.spec(requestSpec)
-											.pathParam("orderId", orderId)
-										.when()
-											.get(IPetStoreEndpoint.GET_ORDER_BY_ID);
-				
-				response.then().log().all();
-				Assertions.setResponse(response);
-		}
-	}
-	
-	@When("user sends DELETE request to delete order by existing id")
-	public void deleteOrderByExistingId() {
-		for(Integer orderId : orderIds) {
-			response = RestAssured
-								.given()
-									.spec(requestSpec)
-									.pathParam("orderId", orderId)
-								.when()
-									.delete(IPetStoreEndpoint.DELETE_ORDER_ID);
-			Assertions.setResponse(response);
-		}
-	}
-	
-	@When("user sends POST request to create a order with invalid id {string}")
-	public void createOrderByInvalidIdType(String id) {
-		
-		response = RestAssured
-								.given()
-									.spec(requestSpec)
-									.body("{\r\n"
-											+ "  \"id\": "+id+",\r\n"
-											+ "  \"petId\": 1001,\r\n"
-											+ "  \"quantity\": 2\r\n"
-											+ "}")
-								.when()
-									.post(IPetStoreEndpoint.CREATE_ORDER);
+		response = RestAssured.given().spec(requestSpec)
+				.body("{\r\n" + "  \"id\": 10002,\r\n" + "  \"petId\": 101,\r\n" + "  \"quantity\": 3\r\n" + "}").when()
+				.post(IPetStoreEndpoint.CREATE_ORDER);
 
 		response.then().log().all();
 		Assertions.setResponse(response);
 	}
-	
-	@When("user sends POST request to create a order with missing mandatory field")
-	public void createOrderByMissingMandatoryField() {
-		response = RestAssured
-							.given()
-								.spec(requestSpec)
-								.body("{\r\n"
-										+ "  \"id\": 1001,\r\n"
-										+ "  \"quantity\": 3\r\n"
-										+ "}")
-							.when()
-								.post(IPetStoreEndpoint.CREATE_ORDER);
-			
-			response.then().log().all();
-			Assertions.setResponse(response);
-	}
-	
-	@When("user sends GET request to fetch the order with invalid order id {string}")
-	public void getOrderByInvaidOrderId(String orderId) {
-		response = RestAssured
-										.given()
-											.spec(requestSpec)
-											.pathParam("orderId", orderId)
-										.when()
-											.get(IPetStoreEndpoint.GET_ORDER_BY_ID);
-		
+
+	@When("user sends GET request to get order by existing id")
+	public void getOrderByExistingId() {
+
+		response = RestAssured.given().spec(requestSpec).pathParam("orderId", orderId).when()
+				.get(IPetStoreEndpoint.GET_ORDER_BY_ID);
+
 		response.then().log().all();
 		Assertions.setResponse(response);
 	}
-	
-	@When("user sends GET request to fetch the order with non existing order id")
-	public void getOrderByNonExistValidOrderId() {
-		for(Integer orderId : orderIds) {
-			response = RestAssured
-							.given()
-								.spec(requestSpec)
-								.pathParam("orderId", orderId)
-							.when()
-								.get(IPetStoreEndpoint.GET_ORDER_BY_ID);
-			
-			response.then().log().all();
-			Assertions.setResponse(response);
-		}
+
+	@When("user sends DELETE request to delete order by existing id")
+	public void deleteOrderByExistingId() {
+		response = RestAssured.given().spec(requestSpec).pathParam("orderId", orderId).when()
+				.delete(IPetStoreEndpoint.DELETE_ORDER_ID);
+		Assertions.setResponse(response);
 	}
-	
-	@When("user sends DELETE request to delete the order with invalid order id {}")
-	public void deleteOrderByNonExistOrderId(Object id) {
-			response = RestAssured
+
+	@When("user sends POST request to create a order with invalid id")
+	public void createOrderByInvalidIdType(DataTable dataTable) {
+
+		Map<String, String> data = dataTable.asMap(String.class, String.class);
+
+		/*
+		 * Map<String, Object> request = new HashMap<>();
+		 * request.put("id",data.get("id")); request.put("petId",
+		 * Integer.parseInt(data.get("petId"))); request.put("quantity",
+		 * Integer.parseInt(data.get("quantity"))); request.put("status",
+		 * data.get("status")); request.put("complete",
+		 * Boolean.parseBoolean(data.get("complete")));
+		 */
+
+		/*
+		 * String request = "{\n" + "  \"id\": \"" + data.get("id") + "\",\n" +
+		 * "  \"petId\": " + Integer.parseInt(data.get("petId")) + ",\n" +
+		 * "  \"quantity\": " + Integer.parseInt(data.get("quantity")) + ",\n" +
+		 * "  \"shipDate\": \"" + data.get("shipDate") + "\",\n" + "  \"status\": \"" +
+		 * data.get("status") + "\",\n" + "  \"complete\": " +
+		 * Boolean.parseBoolean(data.get("complete")) + "\n" + "}";
+		 */
+		
+		String request = "{\r\n"
+				+ "  \"id\": "+data.get("id")+",\r\n"
+				+ "  \"petId\": "+data.get("petId")+",\r\n"
+				+ "  \"quantity\": "+data.get("quantity")+",\r\n"
+				+ "  \"shipDate\": \""+data.get("shipDate")+"\",\r\n"
+				+ "  \"status\": \""+data.get("status")+"\",\r\n"
+				+ "  \"complete\": "+data.get("complete")+"\r\n"
+				+ "}";
+
+		response = RestAssured
 								.given()
 									.spec(requestSpec)
-									.pathParam("orderId", id)
-								.when()
-									.delete(IPetStoreEndpoint.DELETE_ORDER_ID);
-			Assertions.setResponse(response);
+										.body(request)
+									.when()
+										.post(IPetStoreEndpoint.CREATE_ORDER);
+
+		response.then().log().all();
+		Assertions.setResponse(response);
 	}
-	
+
+	@When("user sends POST request to create a order with missing mandatory field")
+	public void createOrderByMissingMandatoryField() {
+		response = RestAssured.given().spec(requestSpec)
+				.body("{\r\n" + "  \"id\": 1001,\r\n" + "  \"quantity\": 3\r\n" + "}").when()
+				.post(IPetStoreEndpoint.CREATE_ORDER);
+
+		response.then().log().all();
+		Assertions.setResponse(response);
+	}
+
+	@When("user sends GET request to fetch the order with invalid order id {string}")
+	public void getOrderByInvaidOrderId(String orderId) {
+		response = RestAssured.given().spec(requestSpec).pathParam("orderId", orderId).when()
+				.get(IPetStoreEndpoint.GET_ORDER_BY_ID);
+
+		response.then().log().all();
+		Assertions.setResponse(response);
+	}
+
+	@When("user sends GET request to fetch the order with non existing order id")
+	public void getOrderByNonExistValidOrderId() {
+		response = RestAssured.given().spec(requestSpec).pathParam("orderId", orderId).when()
+				.get(IPetStoreEndpoint.GET_ORDER_BY_ID);
+
+		response.then().log().all();
+		Assertions.setResponse(response);
+	}
+
+	@When("user sends DELETE request to delete the order with invalid order id {string}")
+	public void deleteOrderByNonExistOrderId(String id) {
+		response = RestAssured.given().spec(requestSpec).pathParam("orderId", id).when()
+				.delete(IPetStoreEndpoint.DELETE_ORDER_ID);
+		Assertions.setResponse(response);
+	}
+
 }
--- src/test/resources/features/Store.feature
+++ src/test/resources/features/Store.feature
@@ -12,31 +12,36 @@ Feature: Manage store operations including inventory and pet orders
 		And Validate "Store" schema
 		And Response status line contains "200 OK"
 		
-	Scenario: Creating the order with valid data 
-		When user sends POST request to place order
+	Scenario Outline: Creating, Fetching and Deleting the order with valid data 
+		When user sends POST request to place order with <rowNum>
 		Then Response status code should be 200
 		And Response time less than 5000 ms
 		And Validate "Order" schema
 		And Response status line contains "200 OK"
 		
-	Scenario: Fetching order with valid order id
 		When user sends GET request to get order by existing id
 		Then Response status code should be 200
 		And Response time less than 5000 ms
 		And Response status line contains "200 OK"
 		
-	Scenario: Deleting order with valid order id
 		When user sends DELETE request to delete order by existing id
 		Then Response status code should be 200
 		And Response time less than 5000 ms
 		And Response status line contains "200 OK"
 		
-	Scenario: Fetching order with deleted order id
 		When user sends GET request to fetch the order with non existing order id
 		Then Response status code should be 404
 		And Response time less than 5000 ms
 		And Validate "Error" schema
 		And Response status line contains "404 Not Found"
+		
+		Examples: 
+		|rowNum|
+		|0|
+		|1|
+		|2|
+		|3|
+		|4|
 		
 	Scenario: Create order with mandatory data
 		When user sends POST request to place order with mandatory field
@@ -45,7 +50,13 @@ Feature: Manage store operations including inventory and pet orders
 		And Response status line contains "200 OK"
 		
 	Scenario: Create order with invalid data 
-		When user sends POST request to create a order with invalid id "abc"
+		When user sends POST request to create a order with invalid id
+		|id|abc|
+		|petId|2001|
+		|quantity|3|
+		|shipDate|2026-04-22T02:30:11.721Z|
+		|status|placed|
+		|complete|true|
 		Then Response status code should be 400
 		And Response time less than 5000 ms
 		And Validate "Error" schema
@@ -66,7 +77,7 @@ Feature: Manage store operations including inventory and pet orders
 		And Response status line contains "400 Bad Request"
 		
 	Scenario Outline: Deleting order with invalid order ID
-		When user sends DELETE request to delete the order with invalid order id <id>
+		When user sends DELETE request to delete the order with invalid order id "<id>"
 		Then Response status code should be 400
 		And Response time less than 5000 ms
 		And Validate "Error" schema
